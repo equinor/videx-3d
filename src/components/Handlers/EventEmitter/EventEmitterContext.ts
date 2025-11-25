d803877aa5795ba34a614cc159c8b60d0b9967fe
Kjerand Pedersen <kjerandp@lyse.net>
2025-11-25 11:20:43 +0100

    feat: Added pointer position and camera to event callback data (#86)

--- src/components/Handlers/EventEmitter/EventEmitterContext.ts
+++ src/components/Handlers/EventEmitter/EventEmitterContext.ts
@@ -1,55 +1,65 @@
 import { createContext, useContext } from 'react'
-import { Object3D } from 'three'
-import { Vec3 } from '../../../sdk'
+import { Camera, Object3D } from 'three'
+import { Vec2, Vec3 } from '../../../sdk'
 
 export type KeysPressed = {
-  altKey: boolean,
-  ctrlKey: boolean,
-  shiftKey: boolean,
+  altKey: boolean
+  ctrlKey: boolean
+  shiftKey: boolean
 }
 
 export type EventEmitterCallbackEvent = {
-  target: Object3D,
-  source: Object3D,
-  instanceIndex?: number,
-  position?: Vec3,
-  keys: KeysPressed,
-  ref: any,
+  target: Object3D
+  source: Object3D
+  instanceIndex?: number
+  position?: Vec3
+  pointer: Vec2
+  camera: Camera
+  keys: KeysPressed
+  ref: any
 }
 
-export type EventEmitterCallback = (event: EventEmitterCallbackEvent) => Promise<void>
+export type EventEmitterCallback = (
+  event: EventEmitterCallbackEvent
+) => Promise<void>
 
 export type Listener = {
-  object: Object3D,
-  ref?: any,
-  handlers: Record<string, EventEmitterCallback>,
+  object: Object3D
+  ref?: any
+  handlers: Record<string, EventEmitterCallback>
 }
 
 export type EventEmitterContextProps = {
-  register: (obj: Object3D, handlers: Record<string, EventEmitterCallback>, ref?: any) => () => void,
+  register: (
+    obj: Object3D,
+    handlers: Record<string, EventEmitterCallback>,
+    ref?: any
+  ) => () => void
 }
 
 /**
  * Context for registering event handlers to the `EventEmitter`
- * 
+ *
  * @see {@link EventEmitter}
- * 
+ *
  * @group Contexts
  */
-export const EventEmitterContext = createContext<EventEmitterContextProps>(null!)
+export const EventEmitterContext = createContext<EventEmitterContextProps>(
+  null!
+)
 
 /**
  * Used to register event handlers for renderable components.
- * 
+ *
  * @example
  * const eventHandler = useEventEmitter()
  *
  * @remarks
  * The generator is an async function that process and returns data required
  * by your components, such as geometry for a mesh.
- * 
+ *
  * @example
- * 
+ *
  * useEffect(() => {
  *   let unregister: (() => void) | null = null
  *   if (eventHandler && ref.current) {
@@ -69,9 +79,9 @@ export const EventEmitterContext = createContext<EventEmitterContextProps>(null!
  *     if (unregister) unregister()
  *   }
  * }, [eventHandler, onPointerClick, onPointerEnter, onPointerLeave, onPointerMove])
- * 
+ *
  * @see {@link EventEmitter}
- * 
+ *
  * @group Hooks
  */
 export const useEventEmitter = () => useContext(EventEmitterContext)
